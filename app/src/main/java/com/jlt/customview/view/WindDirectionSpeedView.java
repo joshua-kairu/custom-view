0a159df53f6c93b72a76f241a27525fcd6c9778d
Joshua Kairu <joshuawk@yandex.com>
2016-09-15 14:04:36 +0300

    3 and 4. Color the space between the circles and draw the north indicator

--- app/src/main/java/com/jlt/customview/view/WindDirectionSpeedView.java
+++ app/src/main/java/com/jlt/customview/view/WindDirectionSpeedView.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -37,16 +38,25 @@ import com.jlt.customview.R;
 public class WindDirectionSpeedView extends View {
 
     /* CONSTANTS */
-    
+
     /* Integers */
-    
+
+    /** The default difference between the radii of the outer and inner circles in dp.  */
+    private static final int DEFAULT_RADIUS_DIFFERENCE_DP = 16;
+
+    /**
+     * The space between the north indicator and the outer circle.
+     * It should be equal to the space between the north indicator and the inner circle. */
+    private static final int NORTH_INDICATOR_VERTICAL_PADDING_DP = 4;
+
     /* Strings */
         
     /* VARIABLES */
 
     /* Paints */
 
-    private Paint mPaint; // ditto
+    private Paint mCirclePaint; // ditto
+    private Paint mNorthIndicatorPaint; // ditto
 
     /* Primitives */
 
@@ -57,10 +67,15 @@ public class WindDirectionSpeedView extends View {
 
     private float mArrowAngle; // ditto
     private float mRadiusDifference; // ditto
+    private float mNorthIndicatorStrokeWidth; // ditto
 
     /* Strings */
 
     private String mNorthIndicatorText; //ditto
+
+    /* Text Paints */
+
+    private TextPaint mTextPaint; // used to measure the text width
 
     /* CONSTRUCTOR */
 
@@ -68,7 +83,7 @@ public class WindDirectionSpeedView extends View {
     public WindDirectionSpeedView( Context context, AttributeSet attrs ) {
 
         // 0. super stuff
-        // 1. initialize paint
+        // 1. initialize paints
         // 2. initialize member variables from the XML
 
         // 0. super stuff
@@ -77,7 +92,11 @@ public class WindDirectionSpeedView extends View {
 
         // 1. initialize paint
 
-        mPaint = new Paint();
+        mCirclePaint = new Paint();
+
+        mTextPaint = new TextPaint( mCirclePaint );
+
+        mNorthIndicatorPaint = new Paint();
 
         // 2. initialize member variables from the XML
 
@@ -96,10 +115,18 @@ public class WindDirectionSpeedView extends View {
                     getResources().getColor( android.R.color.white )
             );
 
+            mRadiusDifference = a.getFloat( R.styleable.WindDirectionSpeedView_radiusDifference,
+                    dpToPx( DEFAULT_RADIUS_DIFFERENCE_DP )
+            );
+
             mNorthIndicatorText = a.getString( R.styleable.WindDirectionSpeedView_northIndicatorText );
 
             mNorthIndicatorColor = a.getColor( R.styleable.WindDirectionSpeedView_northIndicatorColor,
                     getResources().getColor( android.R.color.black )
+            );
+
+            mNorthIndicatorStrokeWidth = a.getFloat(
+                    R.styleable.WindDirectionSpeedView_northIndicatorStrokeWidth, 2.0f
             );
 
             mArrowColor = a.getColor( R.styleable.WindDirectionSpeedView_arrowColor,
@@ -107,10 +134,6 @@ public class WindDirectionSpeedView extends View {
             );
 
             mArrowAngle = a.getFloat( R.styleable.WindDirectionSpeedView_arrowAngle, 0f );
-
-            mRadiusDifference = a.getFloat( R.styleable.WindDirectionSpeedView_radiusDifference,
-                    getResources().getDimension( R.dimen.default_radius_difference )
-            );
 
         } // end trying to get things from XML
 
@@ -160,7 +183,7 @@ public class WindDirectionSpeedView extends View {
     }
 
     // setter for the north indicator color
-    public void setmNorthIndicatorColor( int northIndicatorColor ) {
+    public void setNorthIndicatorColor( int northIndicatorColor ) {
 
         this.mNorthIndicatorColor = northIndicatorColor;
 
@@ -218,6 +241,22 @@ public class WindDirectionSpeedView extends View {
 
     }
 
+    // getter for the north indicator stroke width
+    public float getNorthIndicatorStrokeWidth() {
+        return mNorthIndicatorStrokeWidth;
+    }
+
+    // setter for the north indicator stroke width
+    public void setNorthIndicatorStrokeWidth( float northIndicatorStrokeWidth ) {
+
+        this.mNorthIndicatorStrokeWidth = northIndicatorStrokeWidth;
+
+        // refresh the layout
+        invalidate();
+        requestLayout();
+
+    }
+
     // getter for the radius difference
     public float getRadiusDifference() {
         return mRadiusDifference;
@@ -249,6 +288,15 @@ public class WindDirectionSpeedView extends View {
         // 2a. radius should be less than the outer circle by the radius difference
         // 2b. use the inner circle color
         // 2c. draw
+        // 3. color the radius difference space
+        // 4. write the north indicator
+        // 4a. should be as tall as the radius difference and
+        // the individual vertical indicator padding can allow
+        // 4b. should be on the top of the circle
+        // 4c. should be on the middle of the circle
+        // 4d. should use the right color
+        // 4e. should use the right boldness
+        // 4e. should be drawn
 
         // 0. super stuff
 
@@ -261,38 +309,113 @@ public class WindDirectionSpeedView extends View {
         int viewHalfWidth = ( this.getMeasuredWidth() + getPaddingLeft() + getPaddingRight() ) / 2;
         int viewHalfHeight = ( this.getMeasuredHeight() + getPaddingTop() + getPaddingBottom() ) / 2;
 
-        int radius = -1;
+        int outerRadius = -1;
 
-        if ( viewHalfWidth < viewHalfHeight ) { radius = viewHalfWidth; }
-        else if ( viewHalfHeight < viewHalfWidth ) { radius = viewHalfHeight; }
+        if ( viewHalfWidth < viewHalfHeight ) { outerRadius = viewHalfWidth; }
+        else if ( viewHalfHeight < viewHalfWidth ) { outerRadius = viewHalfHeight; }
 
         // 1b. use the outer circle color
 
-        mPaint.setColor( mOuterCircleColor );
+        mCirclePaint.setColor( mOuterCircleColor );
 
         // 1c. draw
 
-        canvas.drawCircle( viewHalfWidth, viewHalfHeight, radius, mPaint );
+        canvas.drawCircle( viewHalfWidth, viewHalfHeight, outerRadius, mCirclePaint );
 
         // 2. draw the inner circle
 
         // 2a. radius should be less than the outer circle by the radius difference
 
-        radius -= mRadiusDifference;
+        float innerRadius = outerRadius - mRadiusDifference;
 
         // 2b. use the inner circle color
 
-        mPaint.setColor( mInnerCircleColor );
+        mCirclePaint.setColor( mInnerCircleColor );
 
         // 2c. draw
 
-        canvas.drawCircle( viewHalfWidth, viewHalfHeight, radius, mPaint );
+        canvas.drawCircle( viewHalfWidth, viewHalfHeight, innerRadius, mCirclePaint );
 
+        // 3. color the radius difference space -> already done by filling the circles
+
+        // 4. write the north indicator
+
+        // 4a. should be as tall as the radius difference and
+        // the individual vertical indicator padding can allow
+
+        float textHeight = mRadiusDifference - ( dpToPx( NORTH_INDICATOR_VERTICAL_PADDING_DP ) * 2 );
+        mNorthIndicatorPaint.setTextSize( textHeight );
+
+        // 4b. should be on the top of the circle
+
+        // thanks to http://www.programcreek.com/java-api-examples/index.php?source_dir=andbase-master/AndBase/src/com/ab/util/AbGraphicUtil.java
+        // for the TextPaint idea
+        float northIndicatorHeight = mTextPaint.getTextSize();
+
+        float northIndicatorWidth = mTextPaint.measureText( mNorthIndicatorText );
+
+        float northIndicatorY =
+                ( viewHalfHeight - outerRadius ) + // center it vertically on top of the outer circle
+                mRadiusDifference / 2 + // put its baseline halfway the distance from the outer circle to the inner circle
+                ( northIndicatorHeight / 2 ) // put its baseline below the halfway by half the text height
+                ; // thus putting the indicator right in the middle of the circle
+
+        // 4c. should be on the middle of the circle
+
+        float northIndicatorX = viewHalfWidth - northIndicatorWidth / 2.0f;
+
+        // 4d. should use the right color
+
+        mNorthIndicatorPaint.setColor( mNorthIndicatorColor );
+
+        mNorthIndicatorPaint.setStyle( Paint.Style.STROKE );
+
+        mNorthIndicatorPaint.setStrokeWidth( mNorthIndicatorStrokeWidth );
+
+        // 4e. should be drawn
+
+        canvas.drawText( mNorthIndicatorText, northIndicatorX, northIndicatorY, mNorthIndicatorPaint );
 
     } // end onDraw
 
     /* Other Methods */
-    
+
+    /**
+     * Converts the passed in dp value to a pixel float value.
+     *
+     * @param dp    The integer dp value.
+     *
+     * @returns A float representing the pixel value of the passed in dp.
+     * */
+    // method dpToPx
+    private float dpToPx( int dp ) {
+        return ( dp * ( getResources().getDisplayMetrics().densityDpi / 160f ) );
+    }
+
+    /**
+     * Gets the smaller value of the width and the height of this view.
+     * */
+    // begin method getSmallerLength
+    private int getSmallerLength() {
+
+        // 0. get total width
+        // 1. get total height
+        // 2. return the smaller
+
+        // 0. get total width
+
+        int totalWidth = this.getMeasuredWidth() + getPaddingLeft() + getPaddingRight();
+
+        // 1. get total height
+
+        int totalHeight = this.getMeasuredHeight() + getPaddingTop() + getPaddingBottom();
+
+        // 2. return the smaller
+
+        return ( totalWidth < totalHeight ) ? totalWidth : totalHeight;
+
+    } // end method getSmallerLength
+
     /* INNER CLASSES */
 
 } // end class WindDirectionSpeedView
